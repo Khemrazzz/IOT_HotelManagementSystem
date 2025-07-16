3e427a013631990e454d9de69989b526f83de833
Shar5K_95 <145230570+Khemrazzz@users.noreply.github.com>
2025-07-16 10:30:16 +0400

    --

--- src/pages/Login.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Login from './Login';
-
-test('renders login heading', () => {
-  render(<Login />);
-  expect(screen.getByRole('heading', { name: /login/i })).toBeInTheDocument();
-});
