5dcef79b43d5e7fff63faf9a74b23d8013594b7d
Shar5K_95 <145230570+Khemrazzz@users.noreply.github.com>
2025-07-23 01:38:26 +0400

    Add prototype energy dashboard

--- hotal-management-system/src/app/page.tsx
+++ hotal-management-system/src/app/page.tsx
@@ -1,14 +1,6 @@
 import React from 'react';
-import Sidebar from './components/Sidebar';
-import Dashboard from './pages/Dashboard';
+import EnergyMonitoringDashboard from './pages/EnergyMonitoringDashboard';
 
 export default function App() {
-  return (
-    <div className="flex min-h-screen bg-[#f7f4ee]">
-      <Sidebar />
-      <main className="flex-1 p-6">
-        <Dashboard />
-      </main>
-    </div>
-  );
+  return <EnergyMonitoringDashboard />;
 }
